958b026cfc306ee68887c184c50dea3ddc24a97a
Korzun Dmitriy <Korzun.DV@phystech.edu>
2019-10-07 23:48:25 +0300

    Условия заказчика

--- Proekt.py
+++ Proekt.py
@@ -1,15 +1,12 @@
 from graph import *
 from random import *
-
 #---------------------------------
 windowSize(1200, 700)
 canvasSize(1200, 700)
 
-
 # ФОН
 brushColor(0, 0, 0)
 polygon([(0, 0), (0, 700), (1200, 700), (1200, 0)])
-
 
 # БАЗА СТАРКИЛЛЕР
 penSize(5)
@@ -20,7 +17,7 @@ penSize(30)
 penColor(100,100,100)
 strip1 = line(25,350,180,350)
 penColor(0,0,0)
-penSize(10)
+penSize(5)
 strip2 = line(22,350,180,350)
 penColor(0,0,0)
 penSize(5)
@@ -32,9 +29,9 @@ penSize(1)
 starAmount = 20
 count = 0
 starMas = [line(0,0,0,0)]*starAmount
-
 def starLaunch():
     if count2 == 100:
+
         close()
     ran = random()
     brushColor(255, 100 + int(150* ran), 0)
@@ -42,19 +39,16 @@ def starLaunch():
     a = 0.9 * ran + 0.1
     y = 700 * random()
     deleteObject(starMas[count % starAmount])
-    starMas[count % starAmount] = polygon([(1200 + 0 * a, y + 0 * a), (1200 + 25 * a, y + 5 * a), (1200 + 50 * a, y + 5 * a),
-    (1200 + 25 * a, y + 10 * a), (1200 + 80 * a, y + 10 * a), (1200 + 55 * a, y + 5 * a), (1200 + 80 * a, y + 5 * a), (1200 + 80 * a, y - 5 * a),
-    (1200 + 55 * a, y - 5 * a), (1200 + 80 * a, y - 10 * a),  (1200 + 25 * a, y - 10 * a),  (1200 + 50 * a, y - 5 * a), (1200 + 25 * a, y - 5 * a)])
+    starMas[count % starAmount] = polygon([(1200 + 50 * a, y + 0 * a), (1200 + 60 * a, y + 40 * a), (1200 + 100 * a, y + 50 * a), (1200 + 60 * a, y + 60 * a), (1200 + 50 * a, y + 100 * a), (1200 + 40 * a, y + 60 * a), (1200 + 0 * a, y + 50 * a), (1200 + 40 * a, y + 40 * a)])
+
     count += 1
-
 onTimer(starLaunch, 500 )#долет до звезд
-
 
 # ДВИЖЕНИЕ
 def starPush():
     for i in range(starAmount):
-        moveObjectBy(starMas[i], -2-count2 // 4, 0)
 
+        moveObjectBy(starMas[i], -2-count2 // 4, 0)
 onTimer(starPush, 10)
 
 # КОНТРОЛЬ
@@ -63,28 +57,19 @@ def control(event):
     global shot, count2
     # ДВИЖЕНИЕ Старкиллера
     if event.keycode == VK_UP:
-        moveObjectBy(telo, 0, -20)
-        moveObjectBy(strip1, 0, -20)
-        moveObjectBy(strip2, 0, -20)
-        moveObjectBy(gun, 0, -20)
+        moveObjectBy(telo, 0, -14)
+        moveObjectBy(strip1, 0, -14)
+        moveObjectBy(strip2, 0, -14)
+        moveObjectBy(gun, 0, -14)
     if event.keycode == VK_DOWN:
-        moveObjectBy(telo, 0, 20)
-        moveObjectBy(strip1, 0, 20)
-        moveObjectBy(strip2, 0, 20)
-        moveObjectBy(gun, 0, 20)
-    if event.keycode == VK_LEFT:
-        moveObjectBy(telo, -5, 0)
-        moveObjectBy(strip1, -5, 0)
-        moveObjectBy(strip2, -5, 0)
-        moveObjectBy(gun, -5, 0)
-    if event.keycode == VK_RIGHT:
-        moveObjectBy(telo, 5, 0)
-        moveObjectBy(strip1, 5, 0)
-        moveObjectBy(strip2, 5, 0)
-        moveObjectBy(gun, 5, 0)
+        moveObjectBy(telo, 0, 14)
+        moveObjectBy(strip1, 0, 14)
+        moveObjectBy(strip2, 0, 14)
+        moveObjectBy(gun, 0, 14)
+
     # СТРЕЛЬБА
     if event.keycode == VK_SPACE:
-        penSize(10)
+        penSize(30)
         penColor(220,0,0)
         shot = line(150,yCoord(gun)+22,1200,yCoord(gun)+22)
         penColor(0, 0, 0)
@@ -94,17 +79,12 @@ def control(event):
                 deleteObject(starMas[i])
                 starMas[i] = line(0,0,0,0)
                 count2 +=1
-
 onKey(control)
 shot = line(0,0,0,0)
-
 
 # ЗАДЕРЖКА УДАЛЕНИЯ
 def zaderahka():
     global shot
     deleteObject(shot)
-
-onTimer(zaderahka,50)
-
-
+onTimer(zaderahka,120)
 run()
